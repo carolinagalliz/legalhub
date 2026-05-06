dc6f6375fdd073bbc716cf8673fc881c59565e71
CAROLINA LOURDES GALLI ZULUAGA <carolinagalliz@gmail.com>
2026-05-06 15:47:38 -0300

    Add files via upload

--- api/mercadopago-webhook.js
+++ api/mercadopago-webhook.js
@@ -25,22 +25,46 @@ export default async function handler(req, res) {
       return res.status(200).send("Firestore no inicializado");
     }
 
-    if (!req.body) {
-      return res.status(200).send("Webhook activo");
+    const body = req.body || {};
+
+    const paymentId = body?.data?.id || req.query?.["data.id"];
+    const type = body?.type || req.query?.type;
+
+    console.log("PAYMENT ID:", paymentId);
+    console.log("TYPE:", type);
+
+    let email =
+      body?.data?.payer?.email ||
+      body?.payer_email ||
+      body?.data?.email;
+
+    let plan = "activo";
+
+    if (!email && paymentId) {
+      const mpRes = await fetch(`https://api.mercadopago.com/preapproval/${paymentId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}`
+        }
+      });
+
+      const mpData = await mpRes.json();
+
+      console.log("MP DATA:", JSON.stringify(mpData, null, 2));
+
+      email = mpData?.payer_email || mpData?.payer?.email;
+
+      const planId = mpData?.preapproval_plan_id;
+
+      if (planId === "1b14f1ae27a042049d5e180504f6c4d8") {
+        plan = "mensual";
+      }
+
+      if (planId === "5363ed616a1e4681b2eebaf676bfd5d6") {
+        plan = "anual";
+      }
     }
 
-    const body = req.body;
-
-const paymentId = body?.data?.id || req.query?.["data.id"];
-const type = body?.type || req.query?.type;
-
-console.log("PAYMENT ID:", paymentId);
-console.log("TYPE:", type);
-
-const email =
-  body?.data?.payer?.email ||
-  body?.payer_email ||
-  body?.data?.email;
     if (!email) {
       return res.status(200).send("Sin email");
     }
@@ -48,7 +72,7 @@ const email =
     await db.collection("usuarios_autorizados").doc(email.toLowerCase()).set({
       email: email.toLowerCase(),
       activo: true,
-      plan: "activo",
+      plan,
       origen: "mercadopago",
       fechaActivacion: new Date()
     }, { merge: true });
